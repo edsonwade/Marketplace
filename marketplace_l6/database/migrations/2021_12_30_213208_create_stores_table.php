5abc6ec2f68b84918cf22df989a106f40a19ed22
edsonwade <vanilsonmuhongo@gamil.com>
2021-12-30 21:43:26 +0000

    creating table stores with datas

---
+++ marketplace_l6/database/migrations/2021_12_30_213208_create_stores_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stores', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('name');
+            $table->string('slug');
+            $table->string('phone');
+            $table->string('description');
+            $table->timestamps();
+        });
+
+        //Foreign key
+        Schema::table('stores', function (Blueprint $table) {
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');//stores_user_id_foreign
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stores');
+    }
+}
